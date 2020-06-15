2d59867c9b47fe2d39565fcf49cc3116d9e2801a
mefy-pushpendu <pushpendu.ghosh@mefy.care>
2020-06-15 08:03:05 +0530

    multiple contact selection - code imlemented - not wokring though

--- src/app/pages/dashboard/contactpicker/contactpicker.page.ts
+++ src/app/pages/dashboard/contactpicker/contactpicker.page.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ModalController, LoadingController } from "@ionic/angular";
+import {
+  Contact,
+  ContactField,
+  ContactName,
+  Contacts,
+  ContactFindOptions,
+} from "@ionic-native/contacts";
+import { _MatMenuBase } from "@angular/material";
 
 @Component({
-  selector: 'app-contactpicker',
-  templateUrl: './contactpicker.page.html',
-  styleUrls: ['./contactpicker.page.scss'],
+  selector: "app-contactpicker",
+  templateUrl: "./contactpicker.page.html",
+  styleUrls: ["./contactpicker.page.scss"],
 })
 export class ContactpickerPage implements OnInit {
+  public contactList: any = [];
+  public showcontacts: any = [];
+  public search_string: String;
 
-  constructor() { }
+  constructor(public modalCtrl: ModalController, public contacts: Contacts) {}
 
-  ngOnInit() {
+  onInput($event) {
+    let _base = this;
+    if (_base.search_string != null && _base.search_string.length != 0) {
+      this.showcontacts = this.contactList.filter(function (contact) {
+        if (
+          contact.name.toLowerCase().includes(_base.search_string.toLowerCase())
+        ) {
+          return contact;
+        } else if (
+          contact.phone
+            .toLowerCase()
+            .includes(_base.search_string.toLowerCase())
+        ) {
+          return contact;
+        }
+      });
+    } else {
+      this.showcontacts = this.contactList;
+    }
   }
 
+  onCancel($event) {
+    this.showcontacts = this.contactList;
+  }
+
+  ngOnInit() {
+    let _base = this;
+    // this.contacts
+    //   .find(["displayName", "phoneNumbers"], {
+    //     multiple: true,
+    //     hasPhoneNumber: true,
+    //   })
+    //   .then((contacts) => {
+    //     for (var i = 0; i < contacts.length; i++) {
+    //       if (contacts[i].displayName !== null) {
+    //         var contact = {};
+    //         contact["name"] = contacts[i].displayName;
+    //         contact["number"] = contacts[i].phoneNumbers[0].value;
+    //         _base.contactList.push(contact);
+    //       }
+    //       if (i == contacts.length - 1) {
+    //         _base.showcontacts = _base.contactList.map(function(contact){
+    //           contact.select = false;
+    //           return contact;
+    //         });
+    //       }
+    //     }
+    //   });
+  }
+
+  choosecontact(contact){
+    let _base = this;
+    _base.contactList = _base.contactList.map(function(element){
+         if(element.name == contact.name && element.phone == contact.phone){
+            contact.select = !contact.select;
+            return contact;
+         }else{
+           return contact;
+         }
+    });
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalCtrl.dismiss({
+      dismissed: true,
+    });
+  }
 }
--- src/app/pages/dashboard/create-contact-group/create-contact-group.page.ts
+++ src/app/pages/dashboard/create-contact-group/create-contact-group.page.ts
@@ -1,30 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController, LoadingController } from '@ionic/angular';
-import { Contact, ContactField, ContactName, Contacts } from '@ionic-native/contacts';
-import { ContactsList, UserContacts } from 'src/app/models/UserContact';
-import { DataService } from 'src/app/services/DataService';
-import { AlertService } from 'src/app/services/alert.service';
-import { AuthService } from 'src/app/services/auth.service';
-import { EnvService } from 'src/app/services/env.service';
+import { Component, OnInit } from "@angular/core";
+import { ModalController, LoadingController } from "@ionic/angular";
+import {
+  Contact,
+  ContactField,
+  ContactName,
+  Contacts,
+} from "@ionic-native/contacts";
+import { ContactsList, UserContacts } from "src/app/models/UserContact";
+import { DataService } from "src/app/services/DataService";
+import { AlertService } from "src/app/services/alert.service";
+import { AuthService } from "src/app/services/auth.service";
+import { EnvService } from "src/app/services/env.service";
+import { ContactpickerPage } from "./../contactpicker/contactpicker.page";
 
 @Component({
-  selector: 'app-create-contact-group',
-  templateUrl: './create-contact-group.page.html',
-  styleUrls: ['./create-contact-group.page.scss'],
+  selector: "app-create-contact-group",
+  templateUrl: "./create-contact-group.page.html",
+  styleUrls: ["./create-contact-group.page.scss"],
 })
 export class CreateContactGroupPage implements OnInit {
-
   tempContactObj = new UserContacts();
 
-  constructor(private modalController: ModalController,
+  constructor(
+    private modalController: ModalController,
     private contacts: Contacts,
     private dataService: DataService,
     private loadingController: LoadingController,
     private alertService: AlertService,
     private authService: AuthService,
     private env: EnvService
-  ) {
-  }
+  ) {}
 
   ngOnInit() {
     if (this.tempContactObj != null) {
@@ -35,104 +40,114 @@ export class CreateContactGroupPage implements OnInit {
     this.modalController.dismiss();
   }
 
+  async openContactPicker() {
+    let contactPicker = await this.modalController.create({
+      component: ContactpickerPage,
+    });
+    contactPicker.present();
+    const { data } = await contactPicker.onDidDismiss();
+  }
+
   async SaveGroup() {
     if (this.dataService.GetNetworkConnectedStatus()) {
       const loading = await this.loadingController.create({
-        message: 'Please Wait..',
+        message: "Please Wait..",
       });
       loading.present();
-      this.authService.AddUpdateUserContacts(this.tempContactObj)
-        .then(
-          async (res: boolean) => {
-            console.log("response from create contact group -> " + (res));
-            if (res) {
-              loading.dismiss();
-              this.alertService.success("Group Updated Successfully");
-            }
-            else {
-              loading.dismiss();
-              this.alertService.error("Group Update Failed");
-            }
-          })
-        .catch(err => {
+      this.authService
+        .AddUpdateUserContacts(this.tempContactObj)
+        .then(async (res: boolean) => {
+          console.log("response from create contact group -> " + res);
+          if (res) {
+            loading.dismiss();
+            this.alertService.success("Group Updated Successfully");
+          } else {
+            loading.dismiss();
+            this.alertService.error("Group Update Failed");
+          }
+        })
+        .catch((err) => {
           loading.dismiss();
           console.log(err);
-          this.alertService.error("Service End Point Error, Please Check Fields..!");
+          this.alertService.error(
+            "Service End Point Error, Please Check Fields..!"
+          );
         });
-    }
-    else {
+    } else {
       this.alertService.error(this.env.NetworkNotAvailableMsg);
     }
   }
 
   ImportContact() {
-    // this.tempContactObj.ContactList.splice(0)
-    this.contacts.pickContact().then(async (contact: Contact) => {
-      this.tempContactObj.ContactList.push(new ContactsList(contact.displayName, contact.phoneNumbers[0].value));
-      // console.log('The following contact has been selected:' + JSON.stringify(contact1));
-      if (this.dataService.GetNetworkConnectedStatus()) {
-        const loading = await this.loadingController.create({
-          message: 'Please Wait..',
-        });
-        loading.present();
-        this.authService.AddUpdateUserContacts(this.tempContactObj)
-          .then(
-            async (res: boolean) => {
-              console.log("response from create contact group -> " + (res));
-              if (res) {
-                loading.dismiss();
-                this.alertService.success("Group Updated Successfully");
-              }
-              else {
-                loading.dismiss();
-                this.alertService.error("Group Update Failed");
-              }
-            })
-          .catch(err => {
-            loading.dismiss();
-            console.log(err);
-            this.alertService.error("Service End Point Error, Please Check Fields..!");
-          });
-      }
-      else {
-        this.alertService.error(this.env.NetworkNotAvailableMsg);
-      }
-    }, (err) => {
-      console.log('Error: ' + err);
-    })
+    this.openContactPicker();
+    // this.contacts.pickContact().then(
+    //   async (contact: Contact) => {
+    //     this.tempContactObj.ContactList.push(
+    //       new ContactsList(contact.displayName, contact.phoneNumbers[0].value)
+    //     );
+    //     if (this.dataService.GetNetworkConnectedStatus()) {
+    //       const loading = await this.loadingController.create({
+    //         message: "Please Wait..",
+    //       });
+    //       loading.present();
+    //       this.authService
+    //         .AddUpdateUserContacts(this.tempContactObj)
+    //         .then(async (res: boolean) => {
+    //           console.log("response from create contact group -> " + res);
+    //           if (res) {
+    //             loading.dismiss();
+    //             this.alertService.success("Group Updated Successfully");
+    //           } else {
+    //             loading.dismiss();
+    //             this.alertService.error("Group Update Failed");
+    //           }
+    //         })
+    //         .catch((err) => {
+    //           loading.dismiss();
+    //           console.log(err);
+    //           this.alertService.error(
+    //             "Service End Point Error, Please Check Fields..!"
+    //           );
+    //         });
+    //     } else {
+    //       this.alertService.error(this.env.NetworkNotAvailableMsg);
+    //     }
+    //   },
+    //   (err) => {
+    //     console.log("Error: " + err);
+    //   }
+    // );
   }
 
-  editContact() {
-
-  }
+  editContact() {}
 
   async deleteContact(index: number) {
-    this.tempContactObj.ContactList.splice(index, 1)
+    this.tempContactObj.ContactList.splice(index, 1);
     if (this.dataService.GetNetworkConnectedStatus()) {
       const loading = await this.loadingController.create({
-        message: 'Please Wait..',
+        message: "Please Wait..",
       });
       loading.present();
-      this.authService.AddUpdateUserContacts(this.tempContactObj)
-        .then(
-          async (res: boolean) => {
-            console.log("response from create contact group -> " + (res));
-            if (res) {
-              loading.dismiss();
-              this.alertService.success("Group Updated Successfully");
-            }
-            else {
-              loading.dismiss();
-              this.alertService.error("Group Update Failed");
-            }
-          })
-        .catch(err => {
+      this.authService
+        .AddUpdateUserContacts(this.tempContactObj)
+        .then(async (res: boolean) => {
+          console.log("response from create contact group -> " + res);
+          if (res) {
+            loading.dismiss();
+            this.alertService.success("Group Updated Successfully");
+          } else {
+            loading.dismiss();
+            this.alertService.error("Group Update Failed");
+          }
+        })
+        .catch((err) => {
           loading.dismiss();
           console.log(err);
-          this.alertService.error("Service End Point Error, Please Check Fields..!");
+          this.alertService.error(
+            "Service End Point Error, Please Check Fields..!"
+          );
         });
-    }
-    else {
+    } else {
       this.alertService.error(this.env.NetworkNotAvailableMsg);
     }
   }
